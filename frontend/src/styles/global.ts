4c44333b46071c519059f108215f78a428ec7103
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-06-08 20:06:48 -0300

    feat: add ajust in global style

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,12 +1,25 @@
-import { createGlobalStyle, css } from 'styled-components'
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 300;
     font-display: swap;
-    src: local(''), url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
   }
 
   @font-face {
@@ -14,7 +27,8 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: local(''), url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
   }
 
   @font-face {
@@ -22,7 +36,8 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal;
     font-weight: 600;
     font-display: swap;
-    src: local(''), url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
   }
 
   * {
@@ -31,9 +46,14 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
-  ${({ theme }) => css`
+  ${({ theme, removeBg }) => css`
     html {
       font-size: 62.5%;
     }
@@ -41,8 +61,14 @@ const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `}
     }
   `}
+
 `
 
 export default GlobalStyles
