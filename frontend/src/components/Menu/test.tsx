d88978caca6565f80a8fc78f40e167bad1fe721c
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-06-10 21:28:08 -0300

    feat: remove comments

--- frontend/src/components/Menu/test.tsx
+++ frontend/src/components/Menu/test.tsx
@@ -16,19 +16,15 @@ describe('<Menu />', () => {
   it('should handle the open/close mobile menu', () => {
     renderWithTheme(<Menu />)
 
-    // selecionar o nosso MenuFull
     const fullMenuElement = screen.getByRole('navigation', { hidden: true })
 
-    // verificar se o menu tá escondido
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
 
-    // clicar no botão de abrir o menu e verificar se ele abriu
     fireEvent.click(screen.getByLabelText(/open menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
     expect(fullMenuElement).toHaveStyle({ opacity: 1 })
 
-    // clicar no botão de fechar o menu e verificar se ele fechou
     fireEvent.click(screen.getByLabelText(/close menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
