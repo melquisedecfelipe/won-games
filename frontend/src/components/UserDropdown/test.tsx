644cf099be7bfc105791935cb2dbe66436ad44e9
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-06-11 21:26:12 -0300

    feat: add user dropdown

---
+++ frontend/src/components/UserDropdown/test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Melqui" />)
+
+    expect(screen.getByText(/melqui/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Melqui" />)
+
+    userEvent.click(screen.getByText(/melqui/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+})
