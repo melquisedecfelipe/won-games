8ee7664524255338c076472ce988861804ab8834
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-06-12 22:05:32 -0300

    feat: add utils to user local storage

---
+++ frontend/src/utils/localStorage/test.ts
@@ -0,0 +1,28 @@
+import { getStorageItem, setStorageItem } from '.'
+
+describe('getStorageItem()', () => {
+  beforeEach(() => {
+    window.localStorage.clear()
+  })
+  it('should return the item from localStorage', () => {
+    window.localStorage.setItem(
+      '@WonGames:cartItems',
+      JSON.stringify(['1', '2'])
+    )
+
+    expect(getStorageItem('cartItems')).toStrictEqual(['1', '2'])
+  })
+})
+
+describe('setStorageItem()', () => {
+  beforeEach(() => {
+    window.localStorage.clear()
+  })
+  it('should add the item to localStorage', () => {
+    setStorageItem('cartItems', ['1', '2'])
+
+    expect(window.localStorage.getItem('@WonGames:cartItems')).toStrictEqual(
+      JSON.stringify(['1', '2'])
+    )
+  })
+})
