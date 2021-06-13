61461f4d41a9c0b061d1b6180011a0a921410817
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-06-12 21:48:40 -0300

    feat: update revalidate in home and game page

--- frontend/src/pages/game/[slug].tsx
+++ frontend/src/pages/game/[slug].tsx
@@ -48,7 +48,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     QueryGameBySlugVariables
   >({
     query: QUERY_GAME_BY_SLUG,
-    variables: { slug: `${params?.slug}` }
+    variables: { slug: `${params?.slug}` },
+    fetchPolicy: 'no-cache'
   })
 
   if (!data.games.length) {
@@ -68,8 +69,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   >({ query: QUERY_UPCOMING, variables: { date: TODAY } })
 
   return {
+    revalidate: 60,
     props: {
-      revalidate: 60,
       cover: `http://localhost:3333${game.cover?.src}`,
       gameInfo: {
         title: game.name,
