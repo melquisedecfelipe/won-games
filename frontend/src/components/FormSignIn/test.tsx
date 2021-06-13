462b0f256f46bbe0e27067754edb085f7fd83118
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-06-13 19:54:44 -0300

    feat: add sign in and sing up

--- frontend/src/components/FormSignIn/test.tsx
+++ frontend/src/components/FormSignIn/test.tsx
@@ -2,15 +2,28 @@ import { render, screen } from 'utils/test-utils'
 
 import FormSignIn from '.'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
+
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    render(<FormSignIn />)
+    const { container } = render(<FormSignIn />)
 
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
     expect(
       screen.getByRole('button', { name: /sign in now/i })
     ).toBeInTheDocument()
+
+    expect(container.parentElement).toMatchSnapshot()
   })
 
   it('should render the forgot password link', () => {
