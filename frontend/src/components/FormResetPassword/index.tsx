d33e171016dcfcfb3826e8b5dced001b787795fb
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-06-21 18:26:57 -0300

    feat: add form forgot and reset password

--- frontend/src/components/FormResetPassword/index.tsx
+++ frontend/src/components/FormResetPassword/index.tsx
@@ -1,21 +1,19 @@
 import { useState } from 'react'
 import { signIn } from 'next-auth/client'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import { Email, Lock, ErrorOutline } from '@styled-icons/material-outlined'
+import { Lock, ErrorOutline } from '@styled-icons/material-outlined'
 
-import { FormLink, FormWrapper, FormLoading, FormError } from 'components/Form'
+import { FormWrapper, FormLoading, FormError } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
 
-import * as S from './styles'
-import { FieldErrors, signInValidate } from 'utils/validations'
+import { FieldErrors, resetValidate } from 'utils/validations'
 
-const FormSignIn = () => {
+const FormResetPassword = () => {
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
-  const [values, setValues] = useState({ email: '', password: '' })
+  const [values, setValues] = useState({ password: '', confirm_password: '' })
   const [loading, setLoading] = useState(false)
   const routes = useRouter()
   const { push, query } = routes
@@ -28,7 +26,7 @@ const FormSignIn = () => {
     event.preventDefault()
     setLoading(true)
 
-    const errors = signInValidate(values)
+    const errors = resetValidate(values)
 
     if (Object.keys(errors).length) {
       setFieldError(errors)
@@ -62,14 +60,6 @@ const FormSignIn = () => {
       )}
       <form onSubmit={handleSubmit}>
         <TextField
-          name="email"
-          placeholder="Email"
-          type="email"
-          error={fieldError?.email}
-          onInputChange={v => handleInput('email', v)}
-          icon={<Email />}
-        />
-        <TextField
           name="password"
           placeholder="Password"
           type="password"
@@ -77,23 +67,21 @@ const FormSignIn = () => {
           onInputChange={v => handleInput('password', v)}
           icon={<Lock />}
         />
-        <Link href="/forgot-password" passHref>
-          <S.ForgotPassword>Forgot your password?</S.ForgotPassword>
-        </Link>
+        <TextField
+          name="confirm_password"
+          placeholder="Confirm password"
+          type="password"
+          error={fieldError?.confirm_password}
+          onInputChange={v => handleInput('confirm_password', v)}
+          icon={<Lock />}
+        />
 
         <Button type="submit" size="large" fullWidth disabled={loading}>
-          {loading ? <FormLoading /> : <span>Sign in now</span>}
+          {loading ? <FormLoading /> : <span>Reset Password</span>}
         </Button>
-
-        <FormLink>
-          Don’t have an account?{' '}
-          <Link href="/sign-up">
-            <a>Sign up</a>
-          </Link>
-        </FormLink>
       </form>
     </FormWrapper>
   )
 }
 
-export default FormSignIn
+export default FormResetPassword
