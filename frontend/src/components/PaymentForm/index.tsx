14497e14f3c99fc97e8757f18ab0f2b21db0d008
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-08-28 12:53:27 -0300

    feat: update payment form component

--- frontend/src/components/PaymentForm/index.tsx
+++ frontend/src/components/PaymentForm/index.tsx
@@ -1,4 +1,7 @@
-import { ShoppingCart } from '@styled-icons/material-outlined'
+import { useState } from 'react'
+import { CardElement } from '@stripe/react-stripe-js'
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
+import { ErrorOutline, ShoppingCart } from '@styled-icons/material-outlined'
 
 import Button from 'components/Button'
 import Heading from 'components/Heading'
@@ -6,18 +9,49 @@ import Heading from 'components/Heading'
 import * as S from './styles'
 
 const PaymentForm = () => {
+  const [error, setError] = useState<string | null>(null)
+  const [disabled, setDisabled] = useState(true)
+
+  const handleChange = async (event: StripeCardElementChangeEvent) => {
+    setDisabled(event.empty)
+    setError(event.error ? event.error.message : '')
+  }
+
   return (
     <S.Wrapper>
       <S.Body>
         <Heading color="black" size="small" lineBottom>
           Payment
         </Heading>
+
+        <CardElement
+          options={{
+            hidePostalCode: true,
+            style: {
+              base: {
+                fontSize: '16px'
+              }
+            }
+          }}
+          onChange={handleChange}
+        />
+
+        {error && (
+          <S.Error>
+            <ErrorOutline size={20} />
+            {error}
+          </S.Error>
+        )}
       </S.Body>
       <S.Footer>
         <Button as="a" fullWidth minimal>
           Continue shopping
         </Button>
-        <Button fullWidth icon={<ShoppingCart />}>
+        <Button
+          fullWidth
+          icon={<ShoppingCart />}
+          disabled={disabled || !!error}
+        >
           Buy now
         </Button>
       </S.Footer>
