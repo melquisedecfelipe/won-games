27b537da20109205ed89d98a5e7f42ab7db28d14
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2021-08-28 15:34:16 -0300

    feat: add google analytics in app

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import Analytics from 'components/Analytics'
 import Document, {
   Html,
   Head,
@@ -40,6 +41,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     )
